7ae8d52e2d760e89a7833d6dbc720659c357d556
unknown <gespejo2144@gmail.com>
2022-04-03 11:51:32 -0700

    refactored for readability

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-function move(image, left, bottom){
+function move(image){
     image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
+    
+    function moveToCoordinates(left, bottom){
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
 
+    return{
+        to: moveToCoordinates
+    }
 }
 
 function newImage(url){
@@ -12,19 +18,19 @@ function newImage(url){
     return image
 }
 
-let greencharacter = newImage('assets/green-character.gif')
-move(greencharacter, 100, 250)
-let tree = newImage('assets/tree.png')
-let pillar = newImage('assets/pillar.png')
-let pinetree = newImage('assets/pine-tree.png')
-let crate = newImage('assets/crate.png')
-let well = newImage('assets/well.png')
+move(newImage('assets/green-character.gif')).to(100, 250)
 
-move(tree, 200, 450)
-move(pillar, 350, 250)
-move(pinetree, 450, 350)
-move(crate, 150, 350)
-move(well, 500, 575)
+// let tree = 
+// let pillar = 
+// let pinetree = 
+// let crate = 
+// let well = 
+
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
@@ -37,9 +43,9 @@ function newItem(url, left, bottom){
     return item
 }
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
 
 function newInventory(){
     let inventory = document.createElement('div')
